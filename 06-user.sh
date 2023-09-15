ecc57f8450078ce712644ac2f1f9def3443645ec
sharadasd <sharu041@gmail.com>
2023-09-15 14:44:32 +0530

    hello world

---
+++ 06-user.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "please enter your name"
+read -s $USERNAME
+echo "please enter: $USERNAME"
+echo "please enter password"
+read -s $password
+echo "please enter: $password"
--- 06-userpass.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "please enter your name"
-read -s username
-echo "you entered :$username
-
-echo "please enter your password"
-read -s password
-echo "you entered :$password
