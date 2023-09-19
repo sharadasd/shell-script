1d3fdaf770737975a083ce23aed31c41172b2e67
sharadasd <sharu041@gmail.com>
2023-09-19 11:10:04 +0530

    hello world

--- 08-functions.sh
+++ 08-functions.sh
@@ -2,7 +2,7 @@
 number=$1
 if [ $number -gt 10 ]
 then
-    ech0"$number is greater than 10"
+    ech0 "$number is greater than 10"
 else 
-    echo"$number is less than 10"
+    echo "$number is less than 10"
 fi
