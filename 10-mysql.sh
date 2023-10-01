3a49153aaee3839f92689c84a23a1bcf413b1f8c
sharadasd <sharu041@gmail.com>
2023-10-01 21:17:48 +0530

    added mongodb

--- 10-mysql.sh
+++ 10-mysql.sh
@@ -4,8 +4,8 @@ R=\e[31m
 G=\e[32m
 if [ $? -ne 0 ]
 then 
- echo "$R mysql installation error"
+ echo -e "$R mysql installation error"
  exit 1
 else
- echo "$G mysql installation successfull"
+ echo -e "$G mysql installation successfull"
 fi
