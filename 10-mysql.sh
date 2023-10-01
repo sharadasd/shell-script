ca025d5d7e38e57edeb686d53bddee95f7fbca84
sharadasd <sharu041@gmail.com>
2023-10-01 21:16:32 +0530

    added mongodb

--- 10-mysql.sh
+++ 10-mysql.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 yum install mysql -y
+R=\e[31m
+G=\e[32m
 if [ $? -ne 0 ]
 then 
- echo "mysql installation error"
+ echo "$R mysql installation error"
  exit 1
 else
- echo "mysql installation successfull"
+ echo "$G mysql installation successfull"
 fi
