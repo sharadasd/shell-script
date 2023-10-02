1adaa11ae19ef1b9624c0e65a4fd68fd2a8f52fb
sharadasd <sharu041@gmail.com>
2023-10-02 15:48:21 +0530

    added mongodb

--- 2-algorithm.sh
+++ 2-algorithm.sh
@@ -4,7 +4,7 @@ log_dir=/home/centos/shell-script-logs
 DATE=$(date +%F:%H:%M:%s)
 script_name=$0
 log_files=$log_dir/$0-$DATE.log
-files_to_delete=$( find $app_dir -name "*.log" -type f -mtime +14)
+files_to_delete=$( find $app_dir -name "*.log" -type f -mtime +12)
 echo "script started executing at $DATE" &>>log_files
 while read line
  do 
