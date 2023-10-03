f52336c6c3a285bd25e5d407a4e88b08ca147193
sharadasd <sharu041@gmail.com>
2023-10-03 14:56:50 +0530

    disk_usage

--- 3-algorithm.sh
+++ 3-algorithm.sh
@@ -10,13 +10,14 @@ G="\e[32m"
 disk_usage=$(df -hT)|grep -vE 'tmpfs|Filesystem'
 disk_threshold=1
 message=""
+
  while IFS= read line
  do
   usage=$(echo$line | awk '{print $6}' | cut -d % -f1)
   partition=$(echo$line | awk '{print $1}')
   if [$usage -gt $disk_threshold];
    then 
-   message+= "high disk usage on $Partition : $usage\n"
+   message+="high disk usage on $Partition: $usage\n"
   fi
  done <<< $disk_usage
  echo -e "message: $message"
