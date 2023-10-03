244bb3fb1b8eb17a43ecef20a7cec69124fbfd64
sharadasd <sharu041@gmail.com>
2023-10-03 15:03:02 +0530

    disk_usage

--- 3-algorithm.sh
+++ 3-algorithm.sh
@@ -13,8 +13,8 @@ message=""
 
  while IFS= read line
  do
-  usage=$(echo$line | awk '{print $6}' | cut -d % -f1)
-  partition=$(echo$line | awk '{print $1}')
+  usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+  partition=$(echo $line | awk '{print $1}')
   if [ $usage -gt $disk_threshold ];
    then 
    message+="high disk usage on $Partition: $usage\n"
